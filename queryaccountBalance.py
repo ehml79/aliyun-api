9df912c930169d8985df284837dde4578e18e93a
ehml79 <ehml79@foxmail.com>
2019-09-29 22:46:36 +0800

    update queryaccountBalance.py

--- queryaccountBalance.py
+++ queryaccountBalance.py
@@ -4,8 +4,6 @@
 # python3
 
 
-import os
-import sys
 import hashlib
 import hmac
 import base64
@@ -16,25 +14,24 @@ import requests
 
 
 def get_iso8601_time():
-    '''返回iso8601格式的时间'''
-    TIME_ZONE = "GMT"
-    FORMAT_ISO8601 = "%Y-%m-%dT%H:%M:%SZ"
-    return time.strftime(FORMAT_ISO8601, time.gmtime())
+    """返回iso8601格式的时间"""
+    format_iso8601 = "%Y-%m-%dT%H:%M:%SZ"
+    return time.strftime(format_iso8601, time.gmtime())
 
 
 def get_uuid():
-    '''返回uuid'''
+    """返回uuid"""
     return str(uuid.uuid4())
 
 
 def get_parameters(user_param, Action, AccessKeyId, Version):
-    '''
+    """
     拼接参数字典
     user_param: {"RegionId":"cn-beijing", "LoadBalancerName":"test-node1", "AddressType":"intranet", "VSwitchId":"vsw-2zevjlczuvp2mkhhch12x"}
     Action操作例如:CreateLoadBalancer
     AccessKeyId：access key ID
     Version: 接口的版本
-    '''
+    """
     parameters = {}
     parameters['HTTPMethod'] = 'GET'
     parameters['AccessKeyId'] = AccessKeyId
@@ -50,9 +47,9 @@ def get_parameters(user_param, Action, AccessKeyId, Version):
     return parameters
 
 
+
 def get_param(parameters):
-    pass
-    '''把公共参数拼接成字符串'''
+    """把公共参数拼接成字符串"""
     param_str = ''
     for (k, v) in sorted(parameters.items()):
         param_str += "&" + urllib.request.quote(k, safe='') + "=" + urllib.request.quote(v, safe='')
@@ -61,36 +58,36 @@ def get_param(parameters):
 
 
 def get_StringToSign(parameters, param_str):
-    pass
-    '''拼接生成签名的字符串'''
-    StringToSign = parameters['HTTPMethod'] + "&%2F&" + urllib.request.quote(param_str, safe='')
-    return StringToSign
+    """拼接生成签名的字符串"""
+    string_to_sign = parameters['HTTPMethod'] + "&%2F&" + urllib.request.quote(param_str, safe='')
+    return string_to_sign
+
 
 
 def get_signature(StringToSign, AccessKeySecret):
-    '''构建签名'''
+    """构建签名"""
     h = hmac.new(AccessKeySecret.encode(), StringToSign.encode(), hashlib.sha1)
     signature = base64.b64encode(h.digest()).strip()
     return signature
 
 
 def build_request(server_url, param_str, signature, AccessKeySecret):
-    '''拼接url并进行请求'''
+    """拼接url并进行请求"""
     Signature = "Signature=" + urllib.request.quote(signature)
     param = param_str + "&" + Signature
     request_url = server_url + param
     s = requests.get(request_url)
-    print(s.content)
+    return s.content
 
 
 def get_regions(server_url, Action, user_param, AccessKeySecret, AccessKeyId, Version):
-    '''对请求进行模块
+    """对请求进行模块
     server_url： slb.aliyun.com
     Action = 'DescribeRegions'
     AccessKeySecret, AccessKeyId:也就是ak
     user_param = {'LoadBalancerId': 'lb-2zekxu2elibyexxoo9hlw'}
     Version:例如slb的版本是2014-05-15,每个服务都不相同
-    '''
+    """
     server_url = 'https://' + server_url + '/?'
     AccessKeySecret = AccessKeySecret
     AccessKeyId = AccessKeyId
@@ -98,7 +95,9 @@ def get_regions(server_url, Action, user_param, AccessKeySecret, AccessKeyId, Ve
     param_str = get_param(parameters)
     StringToSign = get_StringToSign(parameters, param_str)
     signature = get_signature(StringToSign, AccessKeySecret + '&')
-    build_request(server_url, param_str, signature, AccessKeySecret)
+    data = build_request(server_url, param_str, signature, AccessKeySecret)
+    return data
+
 
 
 if __name__ == "__main__":
@@ -109,4 +108,5 @@ if __name__ == "__main__":
 
     Action = 'QueryAccountBalance'
     user_param = {}
-    get_regions(server_url, Action, user_param, AccessKeySecret, AccessKeyId, Version)
+    response = get_regions(server_url, Action, user_param, AccessKeySecret, AccessKeyId, Version)
+    print(response)
